b10a499367cfba8fefb9fd2f20386d50fe15fbc1
Alex Grsm <alexey.grsm@gmail.com>
2022-12-14 17:19:02 +0200

    done

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,8 +1,19 @@
-import Swiper, { FreeMode, Mousewheel, Parallax, Controller } from 'swiper';
-Swiper.use([FreeMode, Mousewheel, Parallax, Controller]);
+import Swiper, { Navigation } from 'swiper';
+Swiper.use([Navigation]);
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  const showSlider = new Swiper('.showcase-carousel', {
+    loop: true,
+    slidesPerView: 3,
+    speed: 1800,
+    centeredSlides: true,
+    navigation: {
+      nextEl: '.showcase-navigation__next',
+      prevEl: '.showcase-navigation__prev',
+    },
+  });
 
+  document.querySelector('video').playbackRate = 2;
 
 });
